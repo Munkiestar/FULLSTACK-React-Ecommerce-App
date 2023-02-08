1934c274cf646efc6a8a8daef449576ff19e546e
Igor <munkieryder@gmail.com>
2023-02-08 09:55:46 +0100

    add: navbar mui icons

--- frontend/src/scenes/home/Home.jsx
+++ frontend/src/scenes/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../global/Navbar.jsx";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
