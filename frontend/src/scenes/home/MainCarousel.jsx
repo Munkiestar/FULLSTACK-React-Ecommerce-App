4256c52fdbc2eb0130726696ed613adffb3d92a5
Igor <munkieryder@gmail.com>
2023-02-08 16:11:21 +0100

    carousel manual fix

--- frontend/src/scenes/home/MainCarousel.jsx
+++ frontend/src/scenes/home/MainCarousel.jsx
@@ -5,6 +5,11 @@ import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { shades } from "../../theme";
 
+import img1 from "../../assets/brooke-cagle-aVT8VkmzML4-unsplash.jpeg";
+import img2 from "../../assets/toa-heftiba-dti56waifB4-unsplash.jpeg";
+import img3 from "../../assets/chris-ghinda-wK2ESlRRZQ8-unsplash.jpeg";
+import img4 from "../../assets/jc-gellidon-JM8TkWJ9UIY-unsplash.jpeg";
+import img5 from "../../assets/larm-rmah-R1Ku62Z7zqE-unsplash.jpeg";
 // imports all images from assets folder
 // const importAll = (r) =>
 //   r.keys().reduce((acc, item) => {
@@ -19,6 +24,7 @@ const MainCarousel = () => {
   // const heroTextureImports = importAll(
   //   requireComponents("../../assets", false, /\.(png|jpe?g|svg)$/)
   // );
+
   return (
     <Carousel
       infinitLoop={true}
@@ -56,6 +62,191 @@ const MainCarousel = () => {
         </IconButton>
       )}
     >
+      <Box
+      // key={`carousel-image-${ind}`}
+      >
+        <img
+          src={img1}
+          // alt={`carousel-${ind}`}
+          style={{
+            width: "100%",
+            height: "700px",
+            objectFit: "cover",
+            backgroundAttachment: "fixed",
+          }}
+        />
+        <Box
+          color="white"
+          padding="20px"
+          borderRadius="1px"
+          textAlign="left"
+          backgroundColor="rgb(0,0,0,.4)"
+          position="absolute"
+          top="46%"
+          left={inNonMobile ? "10%" : "0"}
+          right={inNonMobile ? undefined : "0"}
+          margin={inNonMobile ? undefined : "0 auto"}
+          maxWidth={inNonMobile ? undefined : "240px"}
+        >
+          <Typography color={shades.secondary[200]}>--NEW ITEMS </Typography>
+          <Typography variant="h1">Summer Sale </Typography>
+          <Typography
+            fontWeight="bold"
+            color={shades.secondary[300]}
+            sx={{ textDecoration: "underline" }}
+          >
+            Discover More
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+      // key={`carousel-image-${ind}`}
+      >
+        <img
+          src={img2}
+          // alt={`carousel-${ind}`}
+          style={{
+            width: "100%",
+            height: "700px",
+            objectFit: "cover",
+            backgroundAttachment: "fixed",
+          }}
+        />
+        <Box
+          color="white"
+          padding="20px"
+          borderRadius="1px"
+          textAlign="left"
+          backgroundColor="rgb(0,0,0,.4)"
+          position="absolute"
+          top="46%"
+          left={inNonMobile ? "10%" : "0"}
+          right={inNonMobile ? undefined : "0"}
+          margin={inNonMobile ? undefined : "0 auto"}
+          maxWidth={inNonMobile ? undefined : "240px"}
+        >
+          <Typography color={shades.secondary[200]}>--NEW ITEMS </Typography>
+          <Typography variant="h1">Summer Sale </Typography>
+          <Typography
+            fontWeight="bold"
+            color={shades.secondary[300]}
+            sx={{ textDecoration: "underline" }}
+          >
+            Discover More
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+      // key={`carousel-image-${ind}`}
+      >
+        <img
+          src={img3}
+          // alt={`carousel-${ind}`}
+          style={{
+            width: "100%",
+            height: "700px",
+            objectFit: "cover",
+            backgroundAttachment: "fixed",
+          }}
+        />
+        <Box
+          color="white"
+          padding="20px"
+          borderRadius="1px"
+          textAlign="left"
+          backgroundColor="rgb(0,0,0,.4)"
+          position="absolute"
+          top="46%"
+          left={inNonMobile ? "10%" : "0"}
+          right={inNonMobile ? undefined : "0"}
+          margin={inNonMobile ? undefined : "0 auto"}
+          maxWidth={inNonMobile ? undefined : "240px"}
+        >
+          <Typography color={shades.secondary[200]}>--NEW ITEMS </Typography>
+          <Typography variant="h1">Summer Sale </Typography>
+          <Typography
+            fontWeight="bold"
+            color={shades.secondary[300]}
+            sx={{ textDecoration: "underline" }}
+          >
+            Discover More
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+      // key={`carousel-image-${ind}`}
+      >
+        <img
+          src={img4}
+          // alt={`carousel-${ind}`}
+          style={{
+            width: "100%",
+            height: "700px",
+            objectFit: "cover",
+            backgroundAttachment: "fixed",
+          }}
+        />
+        <Box
+          color="white"
+          padding="20px"
+          borderRadius="1px"
+          textAlign="left"
+          backgroundColor="rgb(0,0,0,.4)"
+          position="absolute"
+          top="46%"
+          left={inNonMobile ? "10%" : "0"}
+          right={inNonMobile ? undefined : "0"}
+          margin={inNonMobile ? undefined : "0 auto"}
+          maxWidth={inNonMobile ? undefined : "240px"}
+        >
+          <Typography color={shades.secondary[200]}>--NEW ITEMS </Typography>
+          <Typography variant="h1">Summer Sale </Typography>
+          <Typography
+            fontWeight="bold"
+            color={shades.secondary[300]}
+            sx={{ textDecoration: "underline" }}
+          >
+            Discover More
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+      // key={`carousel-image-${ind}`}
+      >
+        <img
+          src={img5}
+          // alt={`carousel-${ind}`}
+          style={{
+            width: "100%",
+            height: "700px",
+            objectFit: "cover",
+            backgroundAttachment: "fixed",
+          }}
+        />
+        <Box
+          color="white"
+          padding="20px"
+          borderRadius="1px"
+          textAlign="left"
+          backgroundColor="rgb(0,0,0,.4)"
+          position="absolute"
+          top="46%"
+          left={inNonMobile ? "10%" : "0"}
+          right={inNonMobile ? undefined : "0"}
+          margin={inNonMobile ? undefined : "0 auto"}
+          maxWidth={inNonMobile ? undefined : "240px"}
+        >
+          <Typography color={shades.secondary[200]}>--NEW ITEMS </Typography>
+          <Typography variant="h1">Summer Sale </Typography>
+          <Typography
+            fontWeight="bold"
+            color={shades.secondary[300]}
+            sx={{ textDecoration: "underline" }}
+          >
+            Discover More
+          </Typography>
+        </Box>
+      </Box>
       {/*{Object.values(heroTextureImports).map((texture, ind) => (*/}
       {/*  <Box key={`carousel-image-${ind}`}>*/}
       {/*    <img*/}
